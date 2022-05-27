1b082b574efa644f096f6dfdb4b6c468b13e8fb9
Abdullah Al Tansen <abdullahaltansen899@gmail.com>
2022-05-28 01:28:44 +0600

    update here

--- index.js
+++ index.js
@@ -71,6 +71,9 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Running Genius Server");
 });
+app.get("/hello", (req, res) => {
+  res.send("Hello Updated");
+});
 app.listen(port, () => {
   console.log("Running Genius Server on port", port);
 });
