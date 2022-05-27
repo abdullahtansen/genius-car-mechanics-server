ed1b8c8f8e0ce6a9643132bf7a253b7c8f835b77
Abdullah Al Tansen <abdullahaltansen899@gmail.com>
2022-05-28 01:03:00 +0600

    added port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 // mongodb user:genius-car
 // mongodb pass:Cc0Rjq4kPDqYdpTh
 
